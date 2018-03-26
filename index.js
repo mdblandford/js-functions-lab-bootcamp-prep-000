361b2c26a6b3279dc6d59ef6a381e4fd20fc0393
Michael Blandford <mdblandford@gmail.com>
2018-03-26 14:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 function happyHolidays(){return "Happy holidays!"}
-function happyHolidaysTo(name){return `Happy holidays, ${name}!`} 
-function happyHolidaysTo(holiday, name){return `Happy ${holiday},${name}!`}
+function happyHolidaysTo(name){return `Happy holidays, ${name}!`}
